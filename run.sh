a93d7c576fff197dacaa424f4b53b768228b55a5
Daniel Serkowski <dserkowski@soldevelo.com>
2019-04-16 20:28:48 +0200

    AO-156: Improved env substitution script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-envsubst "`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`" < /usr/share/nginx/html/openlmis.js > temp.js
-cat temp.js > /usr/share/nginx/html/openlmis.js
+if [ ! -f template.js ]; then
+    cat /usr/share/nginx/html/openlmis.js > template.js 
+fi
+envsubst "`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`" < template.js  > /usr/share/nginx/html/openlmis.js
 
 node consul/registration.js -c register -f consul/config.json
 nginx -g 'daemon off;'
