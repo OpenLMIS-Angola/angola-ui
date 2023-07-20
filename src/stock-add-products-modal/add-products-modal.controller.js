1bce75508fc6db0dcaeafde226cc6e2dfd9d4eee
dariapionk <dpionk@soldevelo.com>
2023-07-20 13:45:43 +0200

    ANGOLASUP-806: Implement adding default reason in physical inventory

--- src/stock-add-products-modal/add-products-modal.controller.js
+++ src/stock-add-products-modal/add-products-modal.controller.js
@@ -30,11 +30,11 @@
 
     controller.$inject = ['availableItems', 'messageService', 'modalDeferred', 'orderableGroupService',
         '$scope', 'MAX_INTEGER_VALUE', 'hasPermissionToAddNewLot', 'selectedItems', 'alertService',
-        'moment', 'draft', 'physicalInventoryDraftCacheService'];
+        'moment', 'draft', 'physicalInventoryDraftCacheService', 'useDefaultReason', 'defaultReason'];
 
     function controller(availableItems, messageService, modalDeferred, orderableGroupService,
                         $scope, MAX_INTEGER_VALUE, hasPermissionToAddNewLot, selectedItems, alertService,
-                        moment, draft, physicalInventoryDraftCacheService) {
+                        moment, draft, physicalInventoryDraftCacheService, useDefaultReason, defaultReason) {
         var vm = this;
 
         vm.$onInit = onInit;
@@ -313,6 +313,15 @@
             });
             if (noErrors) {
                 vm.addedItems.forEach(function(item) {
+                    // ANGOLASUP-806: Implement adding default reason in physical inventory
+                    if (useDefaultReason && defaultReason !== null) {
+                        var itemStockAdjustments = [{
+                            quantity: item.quantity,
+                            reason: defaultReason
+                        }];
+                        item.stockAdjustments = itemStockAdjustments;
+                    }
+                    // ANGOLASUP-806: Ends here
                     // ANGOLASUP-825: Fixed inventory saving functionality
                     if (item.$isNewItem && item.id) {
                         selectedItems.push(item);
--- src/stock-physical-inventory-draft/physical-inventory-draft.controller.js
+++ src/stock-physical-inventory-draft/physical-inventory-draft.controller.js
@@ -54,6 +54,9 @@
         // ANGOLASUP-825: Ends here
         vm.quantityChanged = quantityChanged;
         vm.checkUnaccountedStockAdjustments = checkUnaccountedStockAdjustments;
+        // ANGOLASUP-806: Implement adding default reason in physical inventory
+        vm.updateDefaultReason = updateDefaultReason;
+        // ANGOLASUP-806: Ends here
 
         /**
          * @ngdoc property
@@ -73,6 +76,30 @@
                 });
             });
         };
+
+        // ANGOLASUP-806: Implement adding default reason in physical inventory
+        /**
+         * @ngdoc property
+         * @propertyOf stock-physical-inventory-draft.controller:PhysicalInventoryDraftController
+         * @name useDefaultReason
+         * @type {Boolean}
+         *
+         * @description
+         * Holds info if default reason should be used.
+         */
+        vm.useDefaultReason = false;
+
+        /**
+         * @ngdoc property
+         * @propertyOf stock-physical-inventory-draft.controller:PhysicalInventoryDraftController
+         * @name useDefaultReason
+         * @type {Object}
+         *
+         * @description
+         * Holds default reason if default reason is used.
+         */
+        vm.defaultReason = null;
+        // ANGOLASUP-806: Ends here
 
         /**
          * @ngdoc property
@@ -260,7 +287,9 @@
                 notYetAddedItems.push(item);
             });
 
-            addProductsModalService.show(notYetAddedItems, draft).then(function() {
+            //ANGOLASUP-806: Implement adding default reason in physical inventory
+            addProductsModalService.show(notYetAddedItems, draft,
+                vm.useDefaultReason, vm.defaultReason).then(function() {
                 $stateParams.program = vm.program;
                 $stateParams.facility = vm.facility;
                 $stateParams.noReload = true;
@@ -273,6 +302,7 @@
                     reload: $state.current.name
                 });
             });
+            // ANGOLASUP-806: Ends here
         };
 
         /**
@@ -448,6 +478,7 @@
         vm.saveOnPageChange = function() {
             var params = {};
             params.noReload = true;
+            params.defaultReason = vm.defaultReason;
             params.isSubmitted = vm.isSubmitted;
 
             return $q.resolve(params);
@@ -682,6 +713,13 @@
             $stateParams.program = undefined;
             $stateParams.facility = undefined;
 
+            // ANGOLASUP-806: Implement adding default reason in physical inventory
+            if (vm.stateParams.defaultReason !== null) {
+                vm.useDefaultReason = true;
+                vm.defaultReason = vm.stateParams.defaultReason;
+            }
+            // ANGOLASUP-806: Ends here
+
             vm.hasLot = _.any(draft.lineItems, function(item) {
                 return item.lot;
             });
@@ -822,6 +860,12 @@
                 }
             });
         }
+
+        // ANGOLASUP-806: Implement adding default reason in physical inventory
+        function updateDefaultReason() {
+            $stateParams.defaultReason = vm.defaultReason;
+        }
+        // ANGOLASUP-806: Ends here
 
         vm.validateOnPageChange();
 
