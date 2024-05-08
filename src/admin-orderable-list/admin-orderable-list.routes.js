ec346c709d8261f81cc2c076ff87c190534a73e2
olewandowski1 <olewandowski@soldevelo.com>
2024-05-08 14:20:20 +0200

    OAM-127: add additional quarantined parameter to products page

--- src/admin-orderable-list/admin-orderable-list.routes.js
+++ src/admin-orderable-list/admin-orderable-list.routes.js
@@ -26,7 +26,7 @@
         $stateProvider.state('openlmis.administration.orderables', {
             showInNavigation: true,
             label: 'adminOrderableList.products',
-            url: '/orderables?code&name&description&program&page&size&sort',
+            url: '/orderables?code&name&quarantined&description&program&page&size&sort',
             controller: 'OrderableListController',
             templateUrl: 'admin-orderable-list/orderable-list.html',
             controllerAs: 'vm',
@@ -41,7 +41,11 @@
                 },
                 orderables: function(paginationService, OrderableResource, $stateParams) {
                     return paginationService.registerUrl($stateParams, function(stateParams) {
-                        return new OrderableResource().query(stateParams);
+                        var params = angular.copy(stateParams);
+
+                        params.quarantined = true;
+
+                        return new OrderableResource().query(params);
                     });
                 },
                 canAdd: function(authorizationService, permissionService, ADMINISTRATION_RIGHTS) {
